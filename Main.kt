2eab6cb40b10f5ad6599c1169fea0cb174c3a78e
Takeya <37664118+take8@users.noreply.github.com>
2022-01-17 02:38:45 +0900

    Update: ルールを追加しやすい設計にする

--- Main.kt
+++ Main.kt
@@ -6,49 +6,72 @@
  * - 3でも5でも割り切れるとき、FizzBuzz
  */
 
+// オレオレ方針: ルールの追加がしやすい設計を考えてみる
+
 fun main() {
     val start = FizzBuzzNumber(1)
     val end = FizzBuzzNumber(100)
     FizzBuzz(start, end).print()
 }
 
+/**
+ * FizzBuzz
+ */
 class FizzBuzz constructor(_start: FizzBuzzNumber, _end: FizzBuzzNumber) {
     val numberSequence: FizzBuzzNumberSequence
+    val ruleList: FizzBuzzRuleList
 
     init {
         // TODO: 必要なら引数のチェック
-    
+
+        // 数字列の生成
         numberSequence = FizzBuzzNumberSequence(_start, _end)
+        // 変換ルールを生成
+        ruleList = generateRuleList()
     }
 
-    fun print() {
-        numberSequence.print()
+    private fun generateRuleList(): FizzBuzzRuleList {
+        return FizzBuzzRuleList(
+            listOf(
+                FizzBuzzRule(FizzBuzzNumber(3), "Fizz"),
+                FizzBuzzRule(FizzBuzzNumber(5), "Buzz"),
+            )
+        )
+    }
+
+    fun print(): Unit {
+        numberSequence.print(ruleList)
     }
 }
 
+/**
+ * FizzBuzz数列
+ */
 class FizzBuzzNumberSequence constructor(_start: FizzBuzzNumber, _end: FizzBuzzNumber) {
-    val values: List<FizzBuzzNumber>
+    val numbers: List<FizzBuzzNumber>
 
     init {
         // TODO: 必要なら引数のチェック
 
-        values = generateSequence(_start, _end)
+        numbers = generateSequence(_start, _end)
     }
-    
+
     private fun generateSequence(_start: FizzBuzzNumber, _end: FizzBuzzNumber): List<FizzBuzzNumber> {
         return ((_start.value)..(_end.value)).toList().map{
             FizzBuzzNumber(it)
         }
     }
 
-    fun print() {
-        // TODO: iterator使ったらいい感じになる?
-        values.forEach{ value ->
-            value.print()
+    fun print(ruleList: FizzBuzzRuleList): Unit {
+        numbers.forEach{ number ->
+            println(number.toText(ruleList))
         }
     }
 }
 
+/**
+ * FizzBuzz数
+ */
 class FizzBuzzNumber constructor(_value: Int) {
     val value: Int
 
@@ -56,31 +79,60 @@ class FizzBuzzNumber constructor(_value: Int) {
         if (_value <= 0) {
             throw IllegalArgumentException("Not natural number...")
         }
-    
+
         value = _value
     }
-    
-    fun isFizz(): Boolean {
-        return value % 3 === 0
-    }
-    
-    fun isBuzz(): Boolean {
-        return value % 5 === 0
-    }
-    
-    fun print() {
-        var result = ""
 
-        if (isFizz()) {
-            result += "Fizz"
+    fun toText(ruleList: FizzBuzzRuleList): String {
+        // var text = ""
+        // ruleList.rules.forEach{ rule ->
+        //     text += rule.toText(this)
+        // }
+        var text = ruleList.rules.map{ rule ->
+            rule.toText(this)
+        }.joinToString("")
+        // どのルールにもマッチしなかった場合
+        if (text.isEmpty()) {
+            text = value.toString()
         }
-        if (isBuzz()) {
-            result += "Buzz"
-        }
-        if (result.isEmpty()) {
-            result = value.toString()
-        }
+        return text
+    }
+}
 
-        println(result)
+/**
+ * FizzBuzzルールのリスト
+ */
+class FizzBuzzRuleList constructor(_rules: List<FizzBuzzRule>) {
+    val rules: List<FizzBuzzRule>
+
+    init {
+        // TODO: 必要なら引数のチェック
+
+        rules = _rules
+    }
+}
+
+/**
+ * FizzBuzzの変換ルール
+ */
+class FizzBuzzRule constructor(_number: FizzBuzzNumber, _text: String) {
+    // TODO: 第一引数を"条件"を表現できるオブジェクトに変更すれば、より汎用的にできそう
+    val number: FizzBuzzNumber
+    // TODO: textもValue Objectにすべきかも
+    val text: String
+
+    init {
+        // TODO: 必要なら引数のチェック
+
+        number = _number
+        text = _text
+    }
+
+    private fun isMultiple(target: FizzBuzzNumber): Boolean {
+        return target.value % number.value === 0
+    }
+
+    fun toText(target: FizzBuzzNumber): String? {
+        return if (isMultiple(target)) text else ""
     }
 }
