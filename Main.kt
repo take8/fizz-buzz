aa31ac255465150962bc9f72cfd5265e0b6ed8a4
Takeya <37664118+take8@users.noreply.github.com>
2022-01-17 02:34:25 +0900

    Update: クラス化

--- Main.kt
+++ Main.kt
@@ -7,15 +7,80 @@
  */
 
 fun main() {
-    for (i in 1..100) {
-        if (i % 5 === 0 && i % 3 === 0) {
-            println("FizzBuzz")
-        } else if (i % 5 === 0) {
-            println("Buzz")
-        } else if (i % 3 === 0) {
-            println("Fizz")
-        } else {
-            println(i)
+    val start = FizzBuzzNumber(1)
+    val end = FizzBuzzNumber(100)
+    FizzBuzz(start, end).print()
+}
+
+class FizzBuzz constructor(_start: FizzBuzzNumber, _end: FizzBuzzNumber) {
+    val numberSequence: FizzBuzzNumberSequence
+
+    init {
+        // TODO: 必要なら引数のチェック
+    
+        numberSequence = FizzBuzzNumberSequence(_start, _end)
+    }
+
+    fun print() {
+        numberSequence.print()
+    }
+}
+
+class FizzBuzzNumberSequence constructor(_start: FizzBuzzNumber, _end: FizzBuzzNumber) {
+    val values: List<FizzBuzzNumber>
+
+    init {
+        // TODO: 必要なら引数のチェック
+
+        values = generateSequence(_start, _end)
+    }
+    
+    private fun generateSequence(_start: FizzBuzzNumber, _end: FizzBuzzNumber): List<FizzBuzzNumber> {
+        return ((_start.value)..(_end.value)).toList().map{
+            FizzBuzzNumber(it)
         }
+    }
+
+    fun print() {
+        // TODO: iterator使ったらいい感じになる?
+        values.forEach{ value ->
+            value.print()
+        }
+    }
+}
+
+class FizzBuzzNumber constructor(_value: Int) {
+    val value: Int
+
+    init {
+        if (_value <= 0) {
+            throw IllegalArgumentException("Not natural number...")
+        }
+    
+        value = _value
+    }
+    
+    fun isFizz(): Boolean {
+        return value % 3 === 0
+    }
+    
+    fun isBuzz(): Boolean {
+        return value % 5 === 0
+    }
+    
+    fun print() {
+        var result = ""
+
+        if (isFizz()) {
+            result += "Fizz"
+        }
+        if (isBuzz()) {
+            result += "Buzz"
+        }
+        if (result.isEmpty()) {
+            result = value.toString()
+        }
+
+        println(result)
     }
 }
