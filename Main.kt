262df246fec6a5809ea667a742ed0fd72b8ce2c9
Takeya <37664118+take8@users.noreply.github.com>
2022-01-17 01:57:38 +0900

    Add: main

---
+++ Main.kt
@@ -0,0 +1,21 @@
+/**
+ * FizzBuzz問題
+ * - 1からnまで数え上げる（1, 2, ・・・, n）
+ * - 3で割り切れるとき、Fizz
+ * - 5で割り切れるとき、Buzz
+ * - 3でも5でも割り切れるとき、FizzBuzz
+ */
+
+fun main() {
+    for (i in 1..100) {
+        if (i % 15 === 0) {
+            println("FizzBuzz")
+        } else if (i % 5 === 0) {
+            println("Buzz")
+        } else if (i % 3 === 0) {
+            println("Fizz")
+        } else {
+            println(i)
+        }
+    }
+}
